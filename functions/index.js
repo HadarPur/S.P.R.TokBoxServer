a1042dd1b6afaafca4bb49e90cd27c4eb18746a1
hpur <hpur@liveperson.com>
2019-05-22 22:26:47 +0300

    first commit

---
+++ functions/index.js
@@ -0,0 +1,42 @@
+const functions = require('firebase-functions');
+const OpenTok = require('opentok');
+
+const apiKey = '46328822';
+const apiSecret = '1031919db407a48f966d8a2e55bc09dce0f43d95';
+const opentok = new OpenTok(apiKey, apiSecret);
+
+exports.opentokSessionId = functions.https.onRequest((request, response) => {
+  opentok.createSession({mediaMode:"routed"}, function(err, session) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      //get token publisher
+      var tokenPub = session.generateToken({
+        role :                   'publisher',
+        expireTime :             (new Date().getTime() / 1000)+(7 * 24 * 60 * 60), // in one week
+        data :                   'name=SPR',
+        initialLayoutClassList : ['focus']
+      });
+
+      //get token subscriber
+      var tokenSub = session.generateToken({
+        role :                   'subscriber',
+        expireTime :             (new Date().getTime() / 1000)+(7 * 24 * 60 * 60), // in one week
+        data :                   'name=SPR',
+        initialLayoutClassList : ['focus']
+      });
+
+      var data = {
+        "apiKey": apiKey,
+        "sessionId": session.sessionId,
+        "tokenPublisher": tokenPub,
+        "tokenSubscriber": tokenSub
+      };
+
+      console.log("data sent to http://localhost:%s", 5000)
+      response.setHeader('content-type', 'application/json');
+      response.end(JSON.stringify(data));
+    })
+  });
